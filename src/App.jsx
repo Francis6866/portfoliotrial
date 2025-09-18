25aa517ff4a7c1945ff5d86aebed4be93599ce8a
Francis james <ibolokefrancisjames2@gmail.com>
2025-09-18 11:34:37 +0100

    added the contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Services from './components/Service/Services'
 import MyWork from './components/MyWork/MyWork'
+import Contact from './components/Contact/Contact'
 // 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <About />
       <Services />
       <MyWork />
+      <Contact />
     </div>
   )
 }
